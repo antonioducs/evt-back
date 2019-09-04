e40fead300b7aa5007fb60d493040cf154fda8c6
antonioducs <antonioduarte.cs@gmail.com>
2019-09-04 18:49:17 -0300

    corr

--- src/routes.js
+++ src/routes.js
@@ -2,21 +2,6 @@ const express = require("express");
 const routes = express.Router();
 const AcessController = require("./controllers/AcessController");
 
-<<<<<<< HEAD
-routes.get("/admin", AdminController.index);
-routes.post("/admin", AdminController.store);
-routes.get("/admin/:id", AdminController.show);
-routes.put("/admin/:id", AdminController.update);
-routes.delete("/admin/:id", AdminController.delete);
-
-routes.get("/insc", InscricaoController.index);
-//routes.post("/insc", InscricaoController.store);
-//routes.get("/insc/:id", InscricaoController.show);
-//routes.put("/insc/:id", InscricaoController.update);
-//routes.delete("/insc/:id", InscricaoController.delete);
-=======
->>>>>>> 58a689593110a2d520ce1988017953f1a1adf197
-
 
 routes.post("/acess", AcessController.login);
 routes.post("/insc", AcessController.getInsc);
